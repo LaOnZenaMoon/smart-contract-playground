5be97559a070e6461ad41e077d11181f290f9d9d
junlee <laonzenamoon@gmail.com>
2022-07-15 17:48:02 +0900

    test: Custom Token sale to purchase 프로세스 테스트 코드 추가

--- application/smart-contract-service-app/src/test/java/me/lozm/app/Web3jTest.java
+++ application/smart-contract-service-app/src/test/java/me/lozm/app/Web3jTest.java
@@ -50,19 +50,24 @@ class Web3jTest {
     @Test
     void invokeSmartContractFunction_success() throws IOException, ExecutionException, InterruptedException {
         // Given
-        final String eoaPrivateKey = "4334f409334858cae7cb2413c393f3c6435324411c8e0cdf9f987f209ccb654d";
-        Credentials callerCredentials = Credentials.create(eoaPrivateKey);
+        final String mintTokenEoaPrivateKey = "4334f409334858cae7cb2413c393f3c6435324411c8e0cdf9f987f209ccb654d";
+        Credentials mintTokenCallerCredentials = Credentials.create(mintTokenEoaPrivateKey);
 
-        Web3j web3j = Web3j.build(new HttpService());
-        Request<?, EthGetTransactionCount> ethGetTransactionCountRequest = web3j.ethGetTransactionCount(callerCredentials.getAddress(), DefaultBlockParameterName.LATEST);
+        final String purchaseTokenEoaPrivateKey = "606ec4972e7222e3d05b08e28648fea50feed7d6f0c9549c7960c724907c1a8d";
+        Credentials purchaseTokenCallerCredentials = Credentials.create(purchaseTokenEoaPrivateKey);
+
+        Web3j mintTokenWeb3j = Web3j.build(new HttpService());
+        Request<?, EthGetTransactionCount> mintTokenEthGetTransactionCountRequest = mintTokenWeb3j.ethGetTransactionCount(mintTokenCallerCredentials.getAddress(), DefaultBlockParameterName.LATEST);
+
+        Web3j purchaseTokenWeb3j = Web3j.build(new HttpService());
+        Request<?, EthGetTransactionCount> purchaseTokenEthGetTransactionCountRequest = purchaseTokenWeb3j.ethGetTransactionCount(purchaseTokenCallerCredentials.getAddress(), DefaultBlockParameterName.LATEST);
 
         final BigInteger gas = new BigInteger("1537000");
-        final String mintTokenContractAddress = "0xD1d9fe55f1f3376C2E34e0AEB599521F164Ef8D6";
-        final String saleTokenContractAddress = "0x6D6AB42d3834172aeB81973cC650107f5A625c89";
+        final String mintTokenContractAddress = "0xfDCBf3Fb268FBe3ea60994261CE44E739B3DdF6f";
+        final String saleTokenContractAddress = "0xe28F88E492d42596fF4374dDBC769c969aa1A5b5";
 
         // When
         log.info("1. mint token");
-
         Function mintTokenFunction = new Function(
                 "mintToken",
                 List.of(),
@@ -73,24 +78,23 @@ class Web3jTest {
                     }
                 }));
         Transaction mintTokenFunctionCallTransaction = Transaction.createFunctionCallTransaction(
-                callerCredentials.getAddress(), // from
-                ethGetTransactionCountRequest.send().getTransactionCount(), // nonce
+                mintTokenCallerCredentials.getAddress(), // from
+                mintTokenEthGetTransactionCountRequest.send().getTransactionCount(), // nonce
                 Transaction.DEFAULT_GAS, // gasPrice
                 gas, //gasLimit
                 mintTokenContractAddress, // to
                 FunctionEncoder.encode(mintTokenFunction) // data
         );
-        EthSendTransaction mintTokenTransactionResponse = web3j.ethSendTransaction(mintTokenFunctionCallTransaction).sendAsync().get();
-        String result = mintTokenTransactionResponse.getResult();
-        List<Type> outputList = FunctionReturnDecoder.decode(result, mintTokenFunction.getOutputParameters());
-
+        EthSendTransaction mintTokenTransactionResponse = mintTokenWeb3j.ethSendTransaction(mintTokenFunctionCallTransaction).sendAsync().get();
         //TODO mintToken function 호출에 대한 return 값 받기
         // 1. return tokenId 받아서 테스트 코드 진행시키기
+        List<Type> outputList = FunctionReturnDecoder.decode(mintTokenTransactionResponse.getResult(), mintTokenFunction.getOutputParameters());
+        log.info("mintToken function output: {}", outputList.toString());
 
         log.info("2. set approval for all");
         Transaction setApprovalForAllFunctionCallTransaction = Transaction.createFunctionCallTransaction(
-                callerCredentials.getAddress(), // from
-                ethGetTransactionCountRequest.send().getTransactionCount(), // nonce
+                mintTokenCallerCredentials.getAddress(), // from
+                mintTokenEthGetTransactionCountRequest.send().getTransactionCount(), // nonce
                 Transaction.DEFAULT_GAS, // gasPrice
                 gas, //gasLimit
                 mintTokenContractAddress, // to
@@ -100,29 +104,29 @@ class Web3jTest {
                         List.of(new TypeReference<Type>() {})
                 ) // data
         ));
-        EthSendTransaction setApprovalForAllTransactionResponse = web3j.ethSendTransaction(setApprovalForAllFunctionCallTransaction).sendAsync().get();
+        EthSendTransaction setApprovalForAllTransactionResponse = mintTokenWeb3j.ethSendTransaction(setApprovalForAllFunctionCallTransaction).sendAsync().get();
 
         log.info("3. is approved for all");
         Transaction isApprovedForAllFunctionCallTransaction = Transaction.createFunctionCallTransaction(
-                callerCredentials.getAddress(), // from
-                ethGetTransactionCountRequest.send().getTransactionCount(), // nonce
+                mintTokenCallerCredentials.getAddress(), // from
+                mintTokenEthGetTransactionCountRequest.send().getTransactionCount(), // nonce
                 Transaction.DEFAULT_GAS, // gasPrice
                 gas, //gasLimit
                 mintTokenContractAddress, // to
                 FunctionEncoder.encode(new Function(
                                 "isApprovedForAll",
-                                List.of(new Address(callerCredentials.getAddress()), new Address(saleTokenContractAddress)),
+                                List.of(new Address(mintTokenCallerCredentials.getAddress()), new Address(saleTokenContractAddress)),
                                 List.of(new TypeReference<Type>() {})
                         ) // data
                 ));
-        EthSendTransaction isApprovedForAllTransactionResponse = web3j.ethSendTransaction(isApprovedForAllFunctionCallTransaction).sendAsync().get();
+        EthSendTransaction isApprovedForAllTransactionResponse = mintTokenWeb3j.ethSendTransaction(isApprovedForAllFunctionCallTransaction).sendAsync().get();
 
         log.info("4. set for sale token");
-        final Long tokenId = 1L;
-        final Long tokenPrice = 10L;
+        final long tokenId = 6L;
+        final long tokenPrice = 10L;
         Transaction setForSaleTokenFunctionCallTransaction = Transaction.createFunctionCallTransaction(
-                callerCredentials.getAddress(), // from
-                ethGetTransactionCountRequest.send().getTransactionCount(), // nonce
+                mintTokenCallerCredentials.getAddress(), // from
+                mintTokenEthGetTransactionCountRequest.send().getTransactionCount(), // nonce
                 Transaction.DEFAULT_GAS, // gasPrice
                 gas, //gasLimit
                 saleTokenContractAddress, // to
@@ -132,7 +136,23 @@ class Web3jTest {
                                 List.of(new TypeReference<Type>() {})
                         ) // data
                 ));
-        EthSendTransaction setForSaleTokenTransactionResponse = web3j.ethSendTransaction(setForSaleTokenFunctionCallTransaction).sendAsync().get();
+        EthSendTransaction setForSaleTokenTransactionResponse = mintTokenWeb3j.ethSendTransaction(setForSaleTokenFunctionCallTransaction).sendAsync().get();
+
+        log.info("5. purchase token");
+        Transaction purchaseTokenFunctionCallTransaction = Transaction.createFunctionCallTransaction(
+                purchaseTokenCallerCredentials.getAddress(), // from
+                purchaseTokenEthGetTransactionCountRequest.send().getTransactionCount(), // nonce
+                Transaction.DEFAULT_GAS, // gasPrice
+                gas, //gasLimit
+                saleTokenContractAddress, // to
+                new BigInteger(String.valueOf(tokenPrice)), // msg.value
+                FunctionEncoder.encode(new Function(
+                                "purchaseToken",
+                                List.of(new Uint256(tokenId - 1)),
+                                List.of(new TypeReference<Type>() {})
+                        ) // data
+                ));
+        EthSendTransaction purchaseTokenTransactionResponse = mintTokenWeb3j.ethSendTransaction(purchaseTokenFunctionCallTransaction).sendAsync().get();
 
         // Then
         assertFalse(mintTokenTransactionResponse.hasError());
@@ -144,8 +164,11 @@ class Web3jTest {
         assertFalse(isApprovedForAllTransactionResponse.hasError());
         assertTrue(isNotBlank(isApprovedForAllTransactionResponse.getResult()));
 
-//        assertFalse(setForSaleTokenTransactionResponse.hasError());
-//        assertTrue(isNotBlank(setForSaleTokenTransactionResponse.getResult()));
+        assertFalse(setForSaleTokenTransactionResponse.hasError());
+        assertTrue(isNotBlank(setForSaleTokenTransactionResponse.getResult()));
+
+        assertFalse(purchaseTokenTransactionResponse.hasError());
+        assertTrue(isNotBlank(purchaseTokenTransactionResponse.getResult()));
     }
 
 }

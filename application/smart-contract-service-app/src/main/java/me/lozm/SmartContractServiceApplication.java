8e0547467951a01e12d625d27b536e43a1492389
junlee <laonzenamoon@gmail.com>
2022-07-18 11:01:47 +0900

    fix: spring application 관련 파일 누락 추가

---
+++ application/smart-contract-service-app/src/main/java/me/lozm/SmartContractServiceApplication.java
@@ -0,0 +1,13 @@
+package me.lozm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContractServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartContractServiceApplication.class, args);
+    }
+
+}
